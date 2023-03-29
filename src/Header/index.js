9190a37ba5fceb5d09364e9e0310f5e90946a155
pengweifeng <pengweifeng@shhgxxkj.wecom.work>
2023-03-29 20:43:47 +0800

    changes

--- src/Header/index.js
+++ src/Header/index.js
@@ -4,7 +4,7 @@ export default function Header({signOut, user}) {
   return (
     <div className="header">
       <p className="p1">
-        <span>Hello {user.username} 这里是pwfGPT😄</span>
+        <span>Hello {user.username}! 这里是pwfGPT😄</span>
         <button onClick={signOut}>Sign out</button>
       </p>
     </div>
